a1d3fb9f1dda072de4dbc27b9abdab338d14dfaa
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-12-28 10:12:21 +0530

    Trying Get Method in Springboot

--- basic01/src/main/java/com/basic01/basic01/Basic01Application.java
+++ basic01/src/main/java/com/basic01/basic01/Basic01Application.java
@@ -2,11 +2,26 @@ package com.basic01.basic01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class Basic01Application {
+	@GetMapping("/")
+	public String home() {
+		return "Hello, Spring Boot!";
+	}
+
+	@GetMapping("/hello")
+	public String getMethodName(@RequestParam String param) { // http://localhost:8080/hello?param=Dinesh
+		return new String("Hello, " + param);
+	}
+	
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Basic01Application.class, args);
 	}
 
