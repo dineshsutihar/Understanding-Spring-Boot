9ec66d2c395bff97b28195db3036b7e4019de827
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-12-28 10:12:54 +0530

    Initial Commit with basic springboot templete

---
+++ basic01/src/main/java/com/basic01/basic01/Basic01Application.java
@@ -0,0 +1,13 @@
+package com.basic01.basic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basic01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basic01Application.class, args);
+	}
+
+}
---
+++ basic01/src/test/java/com/basic01/basic01/Basic01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic01.basic01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basic01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
