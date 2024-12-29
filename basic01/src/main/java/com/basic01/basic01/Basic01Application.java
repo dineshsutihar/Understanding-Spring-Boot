89c0d0d8f53b6cfe05a992962d31573e9b718302
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-12-29 12:22:21 +0530

    Playing with Dynamic Path Route

--- basic01/src/main/java/com/basic01/basic01/Basic01Application.java
+++ basic01/src/main/java/com/basic01/basic01/Basic01Application.java
@@ -3,6 +3,7 @@ package com.basic01.basic01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,7 +19,11 @@ public class Basic01Application {
 	public String getMethodName(@RequestParam String param) { // http://localhost:8080/hello?param=Dinesh
 		return new String("Hello, " + param);
 	}
-	
+
+	@GetMapping("/{name}")
+	public String getMethodName2(@PathVariable String name) { // http://localhost:8080/Dinesh
+		return new String("Hello, " + name);
+	}
 
 	public static void main(String[] args) {
 
