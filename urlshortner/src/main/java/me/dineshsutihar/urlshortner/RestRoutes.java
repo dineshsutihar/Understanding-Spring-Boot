4302f81734eadd4683c18499741080a623885da4
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-02-09 15:12:12 +0530

    Adds basic routes which can be modified later

--- urlshortner/src/main/java/me/dineshsutihar/urlshortner/RestRoutes.java
+++ urlshortner/src/main/java/me/dineshsutihar/urlshortner/RestRoutes.java
@@ -1,9 +1,32 @@
 package me.dineshsutihar.urlshortner;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class RestRoutes {
 
+  @GetMapping("/")
+  public String home() {
+    return "Welcome to URL Shortner";
+  }
+
+  @GetMapping("/shorten")
+  public String shortenUrl(@RequestParam String url) {
+    return "Shortened URL";
+  }
+
+  @GetMapping("/expand")
+  public String expandUrl(@RequestParam String url) {
+    return "Expanded URL";
+  }
+
+  @GetMapping("/stats")
+  public String getStats(@RequestParam String url) {
+    return "Stats";
+  }
 
 }
